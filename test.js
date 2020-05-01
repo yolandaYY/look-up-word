f33cf28a874aa6612c40270c6c2a2c19fd36b391
yyj <834929499@qq.com>
2020-05-01 11:12:45 +0800

    version1.0

---
+++ test.js
@@ -0,0 +1,82 @@
+const mock_translateBody = JSON.parse(`{
+    "tSpeakUrl": "http://openapi.youdao.com/ttsapi?q=%E7%BF%BB%E8%AF%91&langType=zh-CHS&sign=05E5E41F8E0DFEF2B0F52CD5B3EBC6AD&salt=1588067911538&voice=4&format=mp3&appKey=2423360539ba5632",
+    "returnPhrase": [
+      "translate"
+    ],
+    "RequestId": "ef505726-3623-43b8-afb6-40fc368862e5",
+    "web": [
+      {
+        "value": [
+          "翻译",
+          "转化",
+          "平移",
+          "协议转换信息"
+        ],
+        "key": "translate"
+      },
+      {
+        "value": [
+          "语音翻译机随身"
+        ],
+        "key": "SayHi Translate"
+      },
+      {
+        "value": [
+          "线上翻译",
+          "移动工具",
+          "平移工具",
+          "内置翻译辅助工具"
+        ],
+        "key": "Translate Tool"
+      }
+    ],
+    "query": "translate",
+    "translation": [
+      "翻译"
+    ],
+    "errorCode": "0",
+    "dict": {
+      "url": "yddict://m.youdao.com/dict?le=eng&q=translate"
+    },
+    "webdict": {
+      "url": "http://m.youdao.com/dict?le=eng&q=translate"
+    },
+    "basic": {
+      "exam_type": [
+        "高中",
+        "IELTS",
+        "初中"
+      ],
+      "us-phonetic": "trænzˈleɪt,trænsˈleɪt",
+      "phonetic": "trænzˈleɪt; trænsˈleɪt",
+      "uk-phonetic": "trænzˈleɪt; trænsˈleɪt",
+      "wfs": [
+        {
+          "wf": {
+            "name": "过去式",
+            "value": "translated"
+          }
+        },
+        {
+          "wf": {
+            "name": "过去分词",
+            "value": "translated"
+          }
+        },
+        {
+          "wf": {
+            "name": "现在分词",
+            "value": "translating"
+          }
+        }
+      ],
+      "uk-speech": "http://openapi.youdao.com/ttsapi?q=translate&langType=en&sign=95C2A0A8AA66C760AE5643C817663A96&salt=1588067911538&voice=5&format=mp3&appKey=2423360539ba5632",
+      "explains": [
+        "vt. 翻译；转化；解释；转变为；调动",
+        "vi. 翻译"
+      ],
+      "us-speech": "http://openapi.youdao.com/ttsapi?q=translate&langType=en&sign=95C2A0A8AA66C760AE5643C817663A96&salt=1588067911538&voice=6&format=mp3&appKey=2423360539ba5632"
+    },
+    "l": "en2zh-CHS",
+    "speakUrl": "http://openapi.youdao.com/ttsapi?q=translate&langType=en&sign=95C2A0A8AA66C760AE5643C817663A96&salt=1588067911538&voice=4&format=mp3&appKey=2423360539ba5632"
+  }`);
